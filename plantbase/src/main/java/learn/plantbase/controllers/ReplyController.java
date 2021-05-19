48b89ef1b49f73d2570ce2a09cabcdd7754ae2bb
Kayti Wiita <kwiita@dev-10.com>
2021-05-19 19:48:54 -0400

    added reply controller

--- plantbase/src/main/java/learn/plantbase/controllers/ReplyController.java
+++ plantbase/src/main/java/learn/plantbase/controllers/ReplyController.java
@@ -1,9 +1,15 @@
 package learn.plantbase.controllers;
 
 import learn.plantbase.domain.ReplyService;
+import learn.plantbase.domain.Result;
+import learn.plantbase.models.Post;
 import learn.plantbase.models.Reply;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -20,5 +26,43 @@ public class ReplyController {
     @GetMapping("/post/{postId}")
     public List<Reply> findByPostId(@PathVariable int postId) {
         return service.findByPostId(postId);
+    }
+
+    @GetMapping("/{replyId}")
+    public Reply findById(@PathVariable int replyId) {
+        return service.findById(replyId);
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> addReply(@RequestBody @Valid Reply reply, BindingResult results) {
+        Result<Reply> result = service.addReply(reply);
+        if (results.hasErrors()) {
+            return new ResponseEntity<>(results.getAllErrors(), HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(result.getPayload(), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{replyId}")
+    public ResponseEntity<Object> editReply(@PathVariable int replyId, @RequestBody @Valid Reply reply,
+                                            BindingResult results) {
+        if (replyId != reply.getReplyId()) {
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
+
+        Result<Reply> result = service.editReply(reply);
+
+        if (results.hasErrors()) {
+            return new ResponseEntity<>(results.getAllErrors(), HttpStatus.BAD_REQUEST);
+        }
+
+        return ErrorResponse.build(result);
+    }
+
+    @DeleteMapping("/{replyId}")
+    public ResponseEntity<Object> deleteById(@PathVariable int postId) {
+        if (service.deleteById(postId)) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
