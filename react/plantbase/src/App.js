e2322d9d3db62b655a95518bea2f0b3f75bc7d2a
Rachel Cuccia <cucciarachel@gmail.com>
2021-05-20 15:27:08 -0400

    added nav bar and plant components to react app

--- react/plantbase/src/App.js
+++ react/plantbase/src/App.js
@@ -1,8 +1,17 @@
+import Nav from './components/Nav';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-
+    <Router>
+      <Nav />
+    </Router>
     </div>
   );
 }
