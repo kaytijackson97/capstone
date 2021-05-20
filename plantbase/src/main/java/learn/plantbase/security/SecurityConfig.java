6d43263013c7e9346e0d1e799b9c2b447ebbd139
Rachel Cuccia <cucciarachel@gmail.com>
2021-05-20 15:00:28 -0400

    added semi-completed security

--- plantbase/src/main/java/learn/plantbase/security/SecurityConfig.java
+++ plantbase/src/main/java/learn/plantbase/security/SecurityConfig.java
@@ -1,61 +1,89 @@
 package learn.plantbase.security;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.http.HttpMethod;
-//import org.springframework.security.authentication.AuthenticationManager;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.config.http.SessionCreationPolicy;
-//import org.springframework.security.core.userdetails.User;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
 
-//@EnableWebSecurity
-//public class SecurityConfig extends WebSecurityConfigurerAdapter {
-//
-//    @Override
-//    @Bean
-//    protected AuthenticationManager authenticationManager() throws Exception {
-//        return super.authenticationManager();
-//    }
-//
-//    @Autowired
-//    private PasswordEncoder encoder;
-//
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-////        User.UserBuilder userBuilder = User.withUsername("user")
-////                .password("user").passwordEncoder(password -> encoder.encode(password))
-////                .roles("USER");
-////
-////        User.UserBuilder adminBuilder = User.withUsername("admin")
-////                .password("admin").passwordEncoder(password -> encoder.encode(password))
-////                .roles("ADMIN");
-//    }
-//
-//    @Bean
-//    public PasswordEncoder getEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.csrf().disable();
-//        http.cors();
-//
-//        // build antMatchers for each path in your controllers.
-//        http.authorizeRequests()
-//                .antMatchers("/authenticate").permitAll()
-//                .antMatchers(HttpMethod.GET, "/api", /*Get by id*/ "/api/*").permitAll() // anybody is able to hit this endpoint
-//                .antMatchers(HttpMethod.POST, "/api").hasAnyRole("USER", "ADMIN")
-//                .antMatchers(HttpMethod.PUT, "/api").hasAnyRole("USER", "ADMIN")
-//                .antMatchers(HttpMethod.DELETE, "/api/*").hasAnyRole("USER", "ADMIN")
-//                .antMatchers("/**" /* any route in this path not explicitly defined above is denied */).denyAll()
-//                .and()
-//                .sessionManagement()
-//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-//    }
-//}
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final JwtConverter converter;
+
+    public SecurityConfig(JwtConverter converter) {
+        this.converter = converter;
+    }
+
+    @Override
+    @Bean
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
+    @Autowired
+    private PasswordEncoder encoder;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        User.UserBuilder userBuilder = User.withUsername("user")
+                .password("user").passwordEncoder(password -> encoder.encode(password))
+                .roles("USER");
+
+        User.UserBuilder adminBuilder = User.withUsername("admin")
+                .password("admin").passwordEncoder(password -> encoder.encode(password))
+                .roles("ADMIN");
+
+        auth.inMemoryAuthentication()
+                .withUser(userBuilder)
+                .withUser(adminBuilder);
+    }
+
+    @Bean
+    public PasswordEncoder getEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.cors();
+
+        // build antMatchers for each path in your controllers.
+        http.authorizeRequests()
+                .antMatchers("/authenticate").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/plants", /*Get by id*/ "/api/plants/*", "/api/plants/byMyGarden/*").permitAll() // anybody is able to hit this endpoint
+                .antMatchers(HttpMethod.POST, "/api/plants").hasAnyRole("USER", "ADMIN")
+                .antMatchers(HttpMethod.PUT, "/api/plants/*").hasAnyRole("USER", "ADMIN")
+                .antMatchers(HttpMethod.DELETE, "/api/plants/*").hasAnyRole("USER", "ADMIN")
+                //my garden
+                .antMatchers(HttpMethod.GET, "/api/my-garden", /*Get by id*/ "/api/my-garden/*", "/api/my-garden/from-user/*").permitAll() // anybody is able to hit this endpoint
+                .antMatchers(HttpMethod.POST, "/api/my-garden").hasAnyRole("USER", "ADMIN")
+                .antMatchers(HttpMethod.PUT, "/api/my-garden/*").hasAnyRole("USER", "ADMIN")
+                .antMatchers(HttpMethod.DELETE, "/api/my-garden/*").hasAnyRole("USER", "ADMIN")
+                //garden
+                .antMatchers(HttpMethod.GET, "/api/garden", /*Get by id*/ "/api/garden/*").permitAll() // anybody is able to hit this endpoint
+                //post
+                .antMatchers(HttpMethod.GET, "/api/post", /*Get by id*/ "/api/post/*", "/api/post/user/*", "/api/post/plant/*").permitAll() // anybody is able to hit this endpoint
+                .antMatchers(HttpMethod.POST, "/api/post").hasAnyRole("USER", "ADMIN")
+                .antMatchers(HttpMethod.PUT, "/api/post/*").hasAnyRole("USER", "ADMIN")
+                .antMatchers(HttpMethod.DELETE, "/api/post/*").hasAnyRole("USER", "ADMIN")
+                //reply
+
+                //role
+
+                //user
+                .antMatchers("/**" /* any route in this path not explicitly defined above is denied */).denyAll()
+                .and()
+                .addFilter(new JwtRequestFilter(authenticationManager(), converter))
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+}
