2e826096875b3655fc4353c3dc6975977223e7f6
Rachel Cuccia <cucciarachel@gmail.com>
2021-05-21 11:32:05 -0400

    added login/logout authcontext stuff

---
+++ react/plantbase/src/components/NotFound.js
@@ -0,0 +1,10 @@
+function NotFound() {
+    return (
+        <div>
+            <h2 className="text-center">404 Not Found</h2>
+            <img className="container-fluid" src="https://media.comicbook.com/2017/04/pokemon-sad-moments-pikachu-crying-990351-1280x0.jpg" alt="loading..."></img>
+        </div>
+    );
+}
+
+export default NotFound;
---
+++ react/plantbase/src/components/contexts/CurrentUser.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const CurrentUser = React.createContext();
+
+
+export default CurrentUser;
